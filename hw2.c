4fb89d6d63c777acf501c822820d93fc1c6abe5b
yaronimoni <yarons1@mail.tau.ac.il>
2024-12-15 17:19:46 +0200

    mutex initilization

--- hw2.c
+++ hw2.c
@@ -1,58 +1,4 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
-
-typedef struct JobQueue { // thread safe queue
-    char* jobs[1024];
-    int front; // position of the first element
-    int rear; // position where the next element will be added
-    int count; // total number of items currently in the queue
-    pthread_mutex_t lock; // binary semaphore
-    pthread_cond_t not_empty; // command variables
-    pthread_cond_t not_full;
-    bool shutdown; // New flag to signal shutdown
-    long long time;
-    bool log_enabled;
-} JobQueue;
-
-typedef struct {
-    JobQueue* queue;
-    int thread_id; // Unique thread ID
-} WorkerArgs;
-
-
-void* worker_thread(void* arg);
-void create_counter_files(int num_counters);
-void create_threads(int num_threads, int* thread_ids,pthread_t* threads, JobQueue* queue);
-void read_lines(FILE* cmdfile, int* thread_ids, pthread_t* threads, JobQueue* queue, int num_threads, long long start_time, bool log_en);
-void init_queue(JobQueue* queue, long long start_time, bool log_enabled);
-void enqueue(JobQueue* queue, const char* job);
-char* dequeue(JobQueue* queue);
-void execute_command(char* cmd, long long start_time, int TID, bool log_enabled);
-void create_thread_files(int num_threads);
-long long get_current_time_in_milliseconds();
-void print_to_log_file(long long curr, char* cmd,int TID);
-void print_to_log_file_end(long long curr, char* cmd,int TID);
-
-pthread_mutex_t file_mutexes[100]; // Array of mutexes for files (assuming a maximum of 100 files)
-
-//handling active threads
-pthread_cond_t active_threades_cond;
-pthread_mutex_t active_threades_mutex;
-int active_threades = 0;
-
-
-
-// Initialize all mutexes
-void initialize_file_mutexes() {
-    for (int i = 0; i < 100; i++) {
-        pthread_mutex_init(&file_mutexes[i], NULL);
-    }
-}
+#include "hw2.h"
 
 int main(int argc, char *argv[]) {
     long long start_time = get_current_time_in_milliseconds(); // save start time of the program
@@ -78,6 +24,7 @@ int main(int argc, char *argv[]) {
     }
 
     init_queue(queue, start_time, log_enabled);
+    initialize_file_mutexes();
     //init global counter mutex and cond
     pthread_mutex_init(&active_threades_mutex, NULL);
     pthread_cond_init(&active_threades_cond, NULL);
@@ -86,6 +33,11 @@ int main(int argc, char *argv[]) {
     if (log_enabled) {
         create_thread_files(num_threads);
         FILE* file = fopen("dispatcher.txt", "w"); // reset the dispatcher logfile
+        if (file == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+        fclose(file);
     }
     create_threads(num_threads,thread_ids,threads, queue);
     read_lines(cmdfile, thread_ids, threads, queue, num_threads, start_time, log_enabled);
@@ -98,14 +50,8 @@ int main(int argc, char *argv[]) {
     }
     queue->shutdown = true; // Signal shutdown only after queue is empty
     pthread_cond_broadcast(&queue->not_empty); // Wake all threads to terminate
+    pthread_cond_broadcast(&queue->not_full);
     pthread_mutex_unlock(&queue->lock);
-    //not sure we need:
-    // pthread_mutex_lock(&active_threades_mutex);
-    // if (active_threades == 0) {
-    //     pthread_cond_broadcast(&active_threades_cond);
-    //     printf("active therds\n");
-    // }
-    // pthread_mutex_unlock(&active_threades_mutex);
 
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
@@ -113,12 +59,15 @@ int main(int argc, char *argv[]) {
 
     free(threads);
     free(queue);
-    pthread_mutex_lock(&queue->lock);
+
     // clean up mutexes
     pthread_mutex_destroy(&queue->lock);
+    pthread_mutex_destroy(&active_threades_mutex);
+    destroy_file_mutexes();
+    //clean up cond
     pthread_cond_destroy(&queue->not_empty);
-    pthread_cond_destroy(&active_threades_cond);
     pthread_cond_destroy(&queue->not_full);
+    pthread_cond_destroy(&active_threades_cond);
 
     return 0;
 }
@@ -230,6 +179,7 @@ void execute_command(char* cmd, long long start_time, int TID, bool log_enabled)
     else if (strcmp(cmd1, "increment") == 0) {
         pthread_mutex_lock(&file_mutexes[number]); // Lock the mutex for the file
         FILE* file = fopen(filename, "r");
+        
         long long int value;
         fscanf(file, "%lld", &value);
         fclose(file);
@@ -257,7 +207,6 @@ void execute_command(char* cmd, long long start_time, int TID, bool log_enabled)
     }
     pthread_mutex_lock(&active_threades_mutex);
     active_threades -= 1;
-    printf("checking active threads:%d\n", active_threades);
     if (active_threades == 0) {
         pthread_cond_broadcast(&active_threades_cond);
         printf("broadcasting\n");
@@ -345,22 +294,13 @@ void read_lines(FILE* cmdfile, int* thread_ids, pthread_t* threads, JobQueue* qu
                     fclose(file);
             }
 
-            //fix me
-            printf("active thereads: %d\n", active_threades);
+            
             pthread_mutex_lock(&active_threades_mutex);
             while (active_threades > 0) {
                 pthread_cond_wait(&active_threades_cond, &active_threades_mutex);
             }
             pthread_mutex_unlock(&active_threades_mutex);
             continue;
-
-            // pthread_mutex_lock(&queue->lock);
-
-            // while (queue->count > 0) {
-            //     pthread_cond_wait(&queue->not_empty, &queue->lock);
-            // }
-            // pthread_mutex_unlock(&queue->lock);
-            // continue;
         }
 
         else if (strcmp(token, "worker") == 0) {
@@ -403,6 +343,9 @@ void print_to_log_file(long long curr_time, char* cmd,int TID) {
     snprintf(filename, sizeof(filename), "thread%04d.txt", TID);
 
     FILE* file = fopen(filename, "a");
+    if (file == NULL) {
+        perror("Error opening log file");
+    }
     fprintf(file, "TIME %lld: START job %s\n", curr_time, cmd);
     fclose(file);
 }
@@ -412,6 +355,9 @@ void print_to_log_file_end(long long curr_time, char* cmd,int TID) {
     snprintf(filename, sizeof(filename), "thread%04d.txt", TID);
 
     FILE* file = fopen(filename, "a");
+    if (file == NULL) {
+        perror("Error opening log file");
+    }
     fprintf(file, "TIME %lld: END job %s\n", curr_time, cmd);
     fclose(file);
 }
---
+++ hw2.h
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+typedef struct JobQueue { // thread safe queue
+    char* jobs[1024];
+    int front; // position of the first element
+    int rear; // position where the next element will be added
+    int count; // total number of items currently in the queue
+    pthread_mutex_t lock; // binary semaphore
+    pthread_cond_t not_empty; // command variables
+    pthread_cond_t not_full;
+    bool shutdown; // New flag to signal shutdown
+    long long time;
+    bool log_enabled;
+} JobQueue;
+
+typedef struct {
+    JobQueue* queue;
+    int thread_id; // Unique thread ID
+} WorkerArgs;
+
+
+void* worker_thread(void* arg);
+void create_counter_files(int num_counters);
+void create_threads(int num_threads, int* thread_ids,pthread_t* threads, JobQueue* queue);
+void read_lines(FILE* cmdfile, int* thread_ids, pthread_t* threads, JobQueue* queue, int num_threads, long long start_time, bool log_en);
+void init_queue(JobQueue* queue, long long start_time, bool log_enabled);
+void enqueue(JobQueue* queue, const char* job);
+char* dequeue(JobQueue* queue);
+void execute_command(char* cmd, long long start_time, int TID, bool log_enabled);
+void create_thread_files(int num_threads);
+long long get_current_time_in_milliseconds();
+void print_to_log_file(long long curr, char* cmd,int TID);
+void print_to_log_file_end(long long curr, char* cmd,int TID);
+
+pthread_mutex_t file_mutexes[100]; // Array of mutexes for files (assuming a maximum of 100 files)
+
+//handling active threads
+pthread_cond_t active_threades_cond;
+pthread_mutex_t active_threades_mutex;
+int active_threades = 0;
+
+
+
+// Initialize all mutexes
+void initialize_file_mutexes() {
+    for (int i = 0; i < 100; i++) {
+        pthread_mutex_init(&file_mutexes[i], NULL);
+    }
+}
+
+// Initialize all mutexes
+void destroy_file_mutexes() {
+    for (int i = 0; i < 100; i++) {
+        pthread_mutex_destroy(&file_mutexes[i]);
+    }
+}
